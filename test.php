bfd6e49e0c546df6311d533e35d2a0dca2eead74
Thomas Parkison <trparky@toms-world.org>
2021-07-06 15:51:32 -0400

    Fixed some additional things in the test suite.

--- test.php
+++ test.php
@@ -7,8 +7,9 @@ $parser->includeWindowsName = true;
 $parser->includeMacOSName = true;
 $parser->parseUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.70 Safari/537.36 Edg/92.0.902.30"); // This calls the parser function in the class object.
 
-if ($parser->knownbrowser) print $parser->fullname;
+if ($parser->knownbrowser) {
+    print "Browser Name: $parser->fullname";
+    print "\n\n";
+    print_r($parser);
+}
 else print "Unknown Web Browser";
-
-print "\n\n";
-print_r($parser);
