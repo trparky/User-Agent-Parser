a694a9e4d352c242329dff062b6752d7d5ddd278
Thomas Parkison <trparky@toms-world.org>
2021-07-06 15:23:56 -0400

    Added test file.

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+include("parseUserAgentString.php");
+$parser = new parseUserAgentStringClass(); // This creates a new instance of this class object.
+$parser->includeAndroidName = true;
+$parser->includeWindowsName = true;
+$parser->includeMacOSName = true;
+$parser->parseUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.70 Safari/537.36 Edg/92.0.902.30"); // This calls the parser function in the class object.
+
+if ($parser->knownbrowser) print $parser->fullname;
+else print "Unknown Web Browser";
+
+print_r($parserArray);
